a4f957111a428d853be24c4bfbe6f00b5a2a8875
R-NAGHA-AKSHAYAA <naghaakshayaa@gmail.com>
2024-10-16 00:32:20 +0530

    header footer done

--- client/src/components/home/featured.js
+++ client/src/components/home/featured.js
@@ -1,9 +1,27 @@
 import React from 'react'
+import Carrousal from 'utils/carrousal'
 
 const Featured = () => {
+
+    const carrouselItem = [
+        {
+            img:'/images/featured/featured_home.jpg',
+            lineOne:'Fender',
+            lineTwo:'Custom Shop',
+            linkTitle:'Shop Now',
+            linkTo:'/shop'
+        },
+        {
+            img:'/images/featured/featured_home_2.jpg',
+            lineOne:'B-Stock',
+            lineTwo:'Awesome discounts',
+            linkTitle:'View offers',
+            linkTo:'/shop'
+        }
+    ]
     return (
-        <div className='featured-container'>
-            carrusal
+        <div className='featured_container'>
+            <Carrousal items={carrouselItem}/>
         </div>
     )
 }
--- client/src/components/navigation/footer.js
+++ client/src/components/navigation/footer.js
@@ -1,11 +1,61 @@
 import React from 'react'
+import ContactIcon from '@mui/icons-material/Contacts'
+import TimelapseIcon from '@mui/icons-material/Timelapse'
+import PhoneIcon from '@mui/icons-material/Phone'
+import EmailIcon from '@mui/icons-material/Email'
 
 const Footer = () => {
     return (
-        <>
-        Footer
-        </>
+        <footer className='bck_b_dark'>
+            <div className='container'>
+                <div className='logo'>
+                    WAVES
+                </div>
+                <div className='wrapper'>
+                    <div className='left'>
+                        <h2>Contact Information</h2>
+                        <div className='business_nfo'>
+                            <div className='tag'>
+                                <ContactIcon/>
+                                <div className='nfo'>
+                                    <div>Address</div>
+                                    <div>Some street 22</div>
+                                </div>
+                            </div>
+                            <div className='tag'>
+                                <PhoneIcon/>
+                                <div className='nfo'>
+                                    <div>Phone</div>
+                                    <div>8939267762</div>
+                                </div>
+                            </div> 
+                            <div className='tag'>
+                                <TimelapseIcon/>
+                                <div className='nfo'>
+                                    <div>Phone</div>
+                                    <div>8939267762</div>
+                                </div>
+                            </div>
+                            <div className='tag'>
+                                <EmailIcon/>
+                                <div className='nfo'>
+                                    <div>Email</div>
+                                    <div>aksh@naghs.com</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className='left'>
+                        <h2>Be the first to know</h2>
+                        <div>
+                            <div>
+                            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </footer>
     )
 }
-
 export default Footer;
