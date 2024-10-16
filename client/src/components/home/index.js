a1ce3c0df3db79edc4ab1f94f09206c9e691ff30
R Nagha Akshayaa <naghaakshayaa@gmail.com>
2024-10-16 14:13:52 +0530

    buttons and promotions components

--- client/src/components/home/index.js
+++ client/src/components/home/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import Featured from './featured'
+import SlimPromotion from 'utils/promotions'
+
+const slimPromotions = {
+    img:'/images/featured/featured_home_3.jpg',
+    lineOne:'Upto 40%',
+    lineTwo:'For second hand',
+    linkTitle:'Shop Now',
+    linkTo:'/shop'
+}
 
 const Home = () => {
     return (
         <div>
         <Featured/>
+        <SlimPromotion items={slimPromotions}/>
         </div>
     )
 }
